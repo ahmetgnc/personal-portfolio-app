a441405ed5bd10fd6fc8a2ce6d68e8e97d760eb5
Ahmet Genc <ahmetxgenc@gmail.com>
2025-04-15 23:54:33 +0300

    feat: add sending email function on contacts section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,36 @@
 import Heading from "./sub/Heading";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    if (!formRef.current) return;
+    const currentDate = new Date().getTime();
+    formRef.current.setAttribute("time", currentDate.toString());
+    emailjs
+      .sendForm(
+        "service_4piwtr3",
+        "template_v3hbwia",
+        formRef.current,
+        "EIwTT0gqQEpPpAq3a"
+      )
+      .then(
+        () => {
+          alert("Message sent successfully!");
+          formRef.current?.reset(); 
+        },
+        (error) => {
+          alert("Failed to send message. " + error.text);
+        }
+      );
+  };
+
   return (
     <div
       id="contact"
@@ -26,30 +54,36 @@ const Contact = () => {
           />
         </motion.div>
         <motion.form
+          ref={formRef}
           initial={{ opacity: 0, x: 150 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.4 }}
           viewport={{ once: true }}
           className="w-[600px] lg:w-[400px] sm:w-full flex flex-col gap-3"
+          onSubmit={sendEmail}
         >
           <div className="w-full flex flex-row  gap-x-3 lg:gap-y-3">
             <input
               type="text"
+              name="name"
               className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none"
               placeholder="Your Name"
             />
             <input
               type="email"
+              name="email"
               className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none"
               placeholder="Your Email"
             />
           </div>
           <input
             type="text"
+            name="title"
             className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none"
             placeholder="Subject"
           />
           <textarea
+            name="message"
             className="max-h-[250px] min-h-[150px] border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none"
             placeholder="Write Me..."
           ></textarea>
